1e1143ed5a076a3891d2649819d097866aec0740
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-06-05 15:24:08 +0530

    getelementbytagname js code added

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 // GET ELEMENTS BY CLASS NAME
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
 
-items[1].textContent='hello 2';
-items[2].style.background='green';
+// items[1].textContent='hello 2';
+// items[2].style.background='green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+
+// GET ELEMENTS BY Tag NAME
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+
+li[1].textContent='hello 2';
+li[2].style.background='green';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
 }
