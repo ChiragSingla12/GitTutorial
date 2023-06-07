362a4f17f080a47b3c14038da7ce639203b29507
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-06-07 15:28:05 +0530

    user details as an object js code added

--- demo1.js
+++ demo1.js
@@ -9,36 +9,47 @@ const userList = document.querySelector('#users');
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-  e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
+    e.preventDefault();
 
-    // Remove error after 3 seconds
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
+    if (nameInput.value === '' || emailInput.value === '') {
+        // alert('Please enter all fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
 
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+        // Remove error after 3 seconds
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        // Create new list item with user
+        const li = document.createElement('li');
 
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+        // Add text node with input values
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
 
-    // Append to ul
-    userList.appendChild(li);
-    var name = nameInput.value;
-    var email = emailInput.value;
+        // Add HTML
+        // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
 
-    // Store the user details in localStorage
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
+        // Append to ul
+        userList.appendChild(li);
+        
+        var name = nameInput.value;
+        var email = emailInput.value;
 
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
-  }
+        // // Store the user details in localStorage
+        // localStorage.setItem('name', name);
+        // localStorage.setItem('email', email);
+
+
+        // Create an object to store the user details
+        var userDetails = {
+            name: name,
+            email: email
+        };
+
+        // Store the user details with the unique key in localStorage
+        localStorage.setItem('user', JSON.stringify(userDetails));
+        
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
